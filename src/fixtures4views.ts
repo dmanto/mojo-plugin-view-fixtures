630b1488086da370b5313deeb35c2451ed195077
Daniel Mantovani <dmanto@gmail.com>
2022-11-16 09:56:11 -0300

    configure lint

--- src/fixtures4views.ts
+++ src/fixtures4views.ts
@@ -1,26 +1,26 @@
-import yaml from 'js-yaml';
+import type {MojoApp} from '@mojojs/core';
 import Path from '@mojojs/path';
-import { MojoApp } from '@mojojs/core';
+import yaml from 'js-yaml';
 
-export default function fixtures4ViewsPlugin(app: MojoApp, options: { route?: string, baseDir?: string }) {
-    const route = options.route ?? '--';
-    const baseDir = options.baseDir ?? 'test/support/fixtures';
-    app.router.get(route, async (ctx) => {
-        ctx.render({ text: "I am in the fixtures4views route" });
-    });
+export default function fixtures4ViewsPlugin(app: MojoApp, options: {route?: string; baseDir?: string}) {
+  const route = options.route ?? '--';
+  const baseDir = options.baseDir ?? 'test/support/fixtures';
+  app.router.get(route, async ctx => {
+    ctx.render({text: `I am in the fixtures4views route, baseDir is ${baseDir}`});
+  });
 }
 
-export const saveFixture = (input: TemplateStringsArray, ...values: any[]) => {
-    const dir = Path.tempDirSync();
-    const file = dir.child('fixture.yml').writeFileSync(input.join());
-    console.log(input);
-    return file.toString();
-}
+export const saveFixture = (input: TemplateStringsArray) => {
+  const dir = Path.tempDirSync();
+  const file = dir.child('fixture.yml').writeFileSync(input.join());
+  console.log(input);
+  return file.toString();
+};
 
 export const getFixture = (absolute: string) => {
-    const content = new Path(absolute).readFileSync('utf8');
-    return yaml.load(content as string);
-}
+  const content = new Path(absolute).readFileSync('utf8');
+  return yaml.load(content as string);
+};
 
 const example = saveFixture`
     session:
